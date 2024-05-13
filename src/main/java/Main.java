174fcac314f1b87c04266e378fe6bae8cb2ffb6c
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-05-13 22:14:49 +0000

    13/05/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     System.out.println("=====================");
     System.out.println("");
     System.out.println("Álbum: AM");
-    System.out.println("Ano de luançamento: 2013");
+    System.out.println("Ano de lançamento: 2013");
     System.out.println("");
     System.out.println("A cada novo disco que lança, o grupo é cuidado de perto por público e crítica e com AM, novo álbum lançado em 2013, não foi diferente, já que o disco tornou-se o mais bem sucedido da carreira de Alex Turner e companhia e irá aparecer em praticamente todas as listas de melhores do ano.");
     System.out.println("");
