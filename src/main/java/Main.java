aa27cc61c849be543f58da7999ce4b13f67c0b45
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-05-13 14:37:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("=====================");
+    System.out.println("Banda: Arctic Monkeys");
+    System.out.println("=====================");
+    System.out.println("");
+    System.out.println("Álbum: AM");
+    System.out.println("Ano de lançamento: 2013");
+    System.out.println("");
+    System.out.println("A cada novo disco que lança, o grupo é cuidado de perto por público e crítica e com AM, novo álbum lançado em 2013, não foi diferente, já que o disco tornou-se o mais bem sucedido da carreira de Alex Turner e companhia e irá aparecer em praticamente todas as listas de melhores do ano.");
+    System.out.println("")
+    System.out.println("=======");
+    System.out.println("Músicas");
+    System.out.println("=======");
+    System.out.println("")
+    
+    
+  }
+}
