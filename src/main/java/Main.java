afcbc2129f4dfbd98baf299e01a4303e52eca244
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-05-13 15:03:04 +0000

    13/05/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,26 @@ public class Main {
     System.out.println("=====================");
     System.out.println("");
     System.out.println("Álbum: AM");
-    System.out.println("Ano de lançamento: 2013");
+    System.out.println("Ano de luançamento: 2013");
     System.out.println("");
     System.out.println("A cada novo disco que lança, o grupo é cuidado de perto por público e crítica e com AM, novo álbum lançado em 2013, não foi diferente, já que o disco tornou-se o mais bem sucedido da carreira de Alex Turner e companhia e irá aparecer em praticamente todas as listas de melhores do ano.");
-    System.out.println("")
+    System.out.println("");
     System.out.println("=======");
     System.out.println("Músicas");
     System.out.println("=======");
-    System.out.println("")
-    
-    
+    System.out.println("");
+    System.out.println("Nome                                           Tempo (minutos)"); 
+    System.out.println("1. Do I Wanna Know?                                  4:32");
+    System.out.println("2. R U Mine?                                         3:22");
+    System.out.println("3. One for the Road                                  3:26");
+    System.out.println("4. Arabella                                          3:27");
+    System.out.println("5. I Want It All                                     3:04");
+    System.out.println("6. No. 1 Party Anthem                                4:03");
+    System.out.println("7. Mad Sounds                                        3:35");
+    System.out.println("8. Fireside                                          3:01");
+    System.out.println("9. Why'd You Only Call Me When You're High           2:41");
+    System.out.println("10. Snap Out of It                                   3:12");
+    System.out.println("11. Knee Socks                                       4:18");
+    System.out.println("12. I Wanna Be Yours                                 3:04");
   }
 }
